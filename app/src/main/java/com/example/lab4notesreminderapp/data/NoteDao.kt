3dfb25db9c737a8e8fce5c2ab4ed02b85285fba3
darshil2711 <darshilkarkar012@gmail.com>
2025-12-06 11:28:15 -0500

    Feat: Implement Room entity, DAO, and database class

--- app/src/main/java/com/example/lab4notesreminderapp/data/NoteDao.kt
+++ app/src/main/java/com/example/lab4notesreminderapp/data/NoteDao.kt
@@ -1,28 +1,22 @@
 package com.example.lab4notesreminderapp.data
 
-
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
-
 
 @Dao
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(note: Note): Long
-
-
-    @Query("SELECT * FROM notes ORDER BY id DESC")
-    fun getAllNotes(): Flow<List<Note>>
-
+    suspend fun insert(note: Note)
 
     @Update
     suspend fun update(note: Note)
 
-
     @Delete
     suspend fun delete(note: Note)
 
+    @Query("SELECT * FROM notes_table WHERE id = :id")
+    fun getNoteById(id: Int): Flow<Note?>
 
-    @Query("SELECT * FROM notes WHERE id = :id LIMIT 1")
-    suspend fun getNoteById(id: Long): Note?
+    @Query("SELECT * FROM notes_table ORDER BY id DESC")
+    fun getAllNotes(): Flow<List<Note>>
 }
