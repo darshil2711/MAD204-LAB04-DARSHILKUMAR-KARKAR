0b0e54af54d7eb48b59ed6baaaab6fb8556ac337
darshil2711 <darshilkarkar012@gmail.com>
2025-12-06 11:37:41 -0500

    Feat: Implement Repository and ViewModel for notes

--- app/src/main/java/com/example/lab4notesreminderapp/data/NoteRepository.kt
+++ app/src/main/java/com/example/lab4notesreminderapp/data/NoteRepository.kt
@@ -1,1 +1,29 @@
+import androidx.compose.foundation.text2.input.delete
+import androidx.compose.foundation.text2.input.insert
+import com.example.lab4notesreminderapp.data.Note
+import com.example.lab4notesreminderapp.data.NoteDao
+
 package com.example.lab4notesreminderapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    suspend fun update(note: Note) {
+        noteDao.update(note)
+    }
+
+    suspend fun delete(note: Note) {
+        noteDao.delete(note)
+    }
+
+    fun getNoteById(id: Int): Flow<Note?> {
+        return noteDao.getNoteById(id)
+    }
+}
